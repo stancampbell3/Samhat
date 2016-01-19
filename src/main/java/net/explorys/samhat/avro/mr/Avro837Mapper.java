8f2fc48921f8b25f5a4a67950727bf95384685bb
Stan Campbell <stan.campbell3@gmail.com>
2016-01-19 09:49:43 -0500

    Add missing MakeAvroDef class (for processing XML stylesheets outlining 837 classes) and add a main method to the Avro837Mapper

--- src/main/java/net/explorys/samhat/avro/mr/Avro837Mapper.java
+++ src/main/java/net/explorys/samhat/avro/mr/Avro837Mapper.java
@@ -1,9 +1,11 @@
 package net.explorys.samhat.avro.mr;
 
 import net.explorys.samhat.z12.r837.Flat837;
+import net.explorys.staging.config.StagingSystemConfig;
 import org.apache.avro.generic.GenericRecord;
 import org.apache.avro.mapred.AvroCollector;
 import org.apache.avro.mapred.AvroMapper;
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FSDataInputStream;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
@@ -77,4 +79,33 @@ public class Avro837Mapper extends AvroMapper<Flat837, GenericRecord> {
         }
     }
 
+    public static void main(String[] args) {
+
+        try {
+
+            if(args.length<4) {
+                System.out.println("Usage: java net.explorys.samhat.avro.mr.Avro837Mapper <flatSchemaPath> <expandedSchemaPath> <flatDataPath> <outputPath>");
+            } else {
+
+                String flatSchemaPath = args[0];
+                String expandedSchemaPath = args[1];
+                String flatDataPath = args[2];
+                String outputPath = args[3];
+
+                Configuration c = new Configuration();
+                c.set("fs.defaultFS", StagingSystemConfig.getStagingFS());
+
+                Avro837Tool job = new Avro837Tool();
+                job.setConf(c);
+                job.setX837FlatSchemaPath(flatSchemaPath);
+                job.setX837ExpandedSchemaPath(expandedSchemaPath);
+                job.setX837FlatDataPath(flatDataPath);
+                job.setOutputPath(outputPath);
+
+                job.run(null);
+            }
+        } catch(Exception e) {
+            throw new RuntimeException("Error in Avro837Mapper", e);
+        }
+    }
 }
